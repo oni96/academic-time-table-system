4d14f4ad2ebd1c35418a9bcf5e01de287ad66fbd
oni96 <pritubera@gmail.com>
2017-09-10 10:23:52 +0530

    Job description added in comments

--- test/CSVOpener/CSVReader.java
+++ test/CSVOpener/CSVReader.java
@@ -6,6 +6,9 @@ public class CSVReader {
     
     public static void main(String[] args) {
         //Indra write the CSVOPening algo here
+        //1 Import files from a folder or root folder
+        //2 Check files with the coloumn headings and verify them to be legal CSVfiles
+        //3 Have seperate classes to store seperate CSVs
     }
     
 }
