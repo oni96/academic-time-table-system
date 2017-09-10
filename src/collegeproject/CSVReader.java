2bd58ead845067953623309ebfac5724120dd8ee
oni96 <pritubera@gmail.com>
2017-09-10 14:26:59 +0530

    function as well

--- src/collegeproject/CSVReader.java
+++ src/collegeproject/CSVReader.java
@@ -1,4 +1,4 @@
-package csvreader;
+package collegeproject;
 import java.io.*;
 
 
@@ -8,22 +8,23 @@ public class CSVReader {
     public static void main(String[] args) {
        
 
-        String csvFile = "C:\\Users\\INDRANIL\\Desktop";
+        String csvFile = "C:\\Users\\Primus.Flash\\Documents\\NetBeansProjects\\college\\test.csv"; //Update folder and file name here
         String csvSplitBy = ",";
-
+        String line;
+        
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
             while ((line = br.readLine()) != null) {
 
                 
-                String[] ltt = line.split(cvsSplitBy);
+                String[] ltt = line.split(csvSplitBy);
 
-                System.out.println("name[var= " + ltt[10] + " , name=" + ltt[10] + "]");
+                System.out.println("name[var= " + ltt[0] + " , name=" + ltt[1] + "]");
 
             }
 
         } catch (IOException e) {
-            
+            System.out.println("No File");
         }
 
     }
